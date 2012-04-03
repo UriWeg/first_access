2ede6caf5567266815361733ac0918713711e968
Uri Weg <Uri.weg@gmail.com>
2012-04-03 10:05:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstaccess::Application.config.secret_token = '914fc306e37809fafa8deba03218cac6778b9e06a2c6591b1a82a88b098fdefaf4869b224bec9fdf054f6a84ff55572ad60e5ad0371dae3cc40f1a8803af27c5'
